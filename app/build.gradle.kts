53dd530a9173d0d46930ce9d175f230efa9731cb
Sanskar-Rijal <rijalsanskar@gmail.com>
2024-09-27 00:05:31 +0545

    version updated to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.note"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
